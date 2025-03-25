39fde0f8a8704c7ba4cba8e96c7683af3e12eb31
adebola <adebolaadelekeagunbiade@gmail.com>
2025-03-25 16:30:49 +0000

    Add files via upload

--- Brackey's Tutorial Finished/First Game/scripts/player.gd
+++ Brackey's Tutorial Finished/First Game/scripts/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 330.0
-const JUMP_VELOCITY = -500.0
+const SPEED = 140.0
+const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
